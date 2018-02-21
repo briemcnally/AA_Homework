ce177098c2a1b78f1cc42af08c23273b7dc21e14
brie_mcnally <brienne.mcnally@gmail.com>
2018-02-20 21:32:44 -0800

    Completed W2D3 homework

---
+++ W2D3/spec/dessert_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef", name: "Brie") }
+  let(:cookie) { Dessert.new("cookie", 20, chef) }
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(cookie.type).to eq("cookie")
+    end
+
+    it "sets a quantity" do
+      expect(cookie.quantity).to eq(20)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(cookie.ingredients).to eq([])
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect{ Dessert.new("cookie", "lots", chef) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      cookie.add_ingredient("flour")
+      expect(cookie.ingredients).to include("flour")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      ingredients = ["flour", "butter", "sugar", "egg", "milk"]
+
+      ingredients.each { |ingredient| cookie.add_ingredient(ingredient) }
+      cookie.mix!
+      expect(cookie.ingredients).not_to eq(ingredients)
+      expect(cookie.ingredients.sort).to eq(ingredients.sort)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      cookie.eat(5)
+      expect(cookie.quantity).to eq(15)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect { cookie.eat(50) }.to raise_error("not enough left!")
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Brie the Amazing Chef")
+      expect(cookie.serve).to eq("Brie the Amazing Chef has made 20 cookies!")
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(cookie)
+      cookie.make_more
+    end
+  end
+end
