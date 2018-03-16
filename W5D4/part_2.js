29241c84bc1cff51e3ab2c83ce9871a37c7bcbdb
brie_mcnally <brienne.mcnally@gmail.com>
2018-03-16 08:13:21 -0700

    Completed W5D4

---
+++ W5D4/part_2.js
@@ -0,0 +1,24 @@
+function fizzBuzz (array) {
+  const fizzBuzzArr = [];
+
+  array.forEach(el => {
+    if ((el % 3 === 0) ^ (el % 5 === 0)) {
+      fizzBuzzArr.push(el);
+    }
+  });
+
+  return fizzBuzzArr;
+}
+
+
+function isPrime (n) {
+  if (n < 2) { return false; }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
